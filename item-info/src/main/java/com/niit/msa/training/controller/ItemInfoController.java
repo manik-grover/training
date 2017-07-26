63bafddbf5f1d5d257b9421e6e3d9ffea749f70a
manik-grover <manik.grover@gmail.com>
2017-07-26 10:37:48 -0700

    Initial Version

---
+++ item-info/src/main/java/com/niit/msa/training/controller/ItemInfoController.java
@@ -0,0 +1,22 @@
+package com.niit.msa.training.controller;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.msa.training.service.ItemInfoServiceImpl;
+
+@RestController
+public class ItemInfoController {
+	
+
+	ItemInfoServiceImpl itemInfoService = new ItemInfoServiceImpl();
+	
+	@GetMapping("/iteminfo")
+	public HashMap<Long, String> getItemInfo() throws Exception {
+		return itemInfoService.getItemInfo();
+	}
+
+}
