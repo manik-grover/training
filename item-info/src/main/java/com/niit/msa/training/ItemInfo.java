abe70a13a0407a9fc0414ab6316aca738ae77432
manik-grover <manik.grover@gmail.com>
2017-07-26 11:53:32 -0700

    Updated

--- item-info/src/main/java/com/niit/msa/training/ItemInfo.java
+++ item-info/src/main/java/com/niit/msa/training/ItemInfo.java
@@ -2,12 +2,10 @@ package com.niit.msa.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker	
 public class ItemInfo {
 
 	public static void main(String[] args) {
--- item-info/src/main/java/com/niit/msa/training/service/ItemInfoServiceImpl.java
+++ item-info/src/main/java/com/niit/msa/training/service/ItemInfoServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 
 import org.springframework.stereotype.Component;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
 @Component
 public class ItemInfoServiceImpl implements ItemInfoService{
 	
