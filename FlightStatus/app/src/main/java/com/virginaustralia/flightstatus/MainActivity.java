871c72f9eb1f3ab3dad32bed488f392748b577c6
Manik Grover <manik.grover@gmail.com>
2018-04-03 18:15:00 +0530

    Initial Version

---
+++ FlightStatus/app/src/main/java/com/virginaustralia/flightstatus/MainActivity.java
@@ -0,0 +1,96 @@
+package com.virginaustralia.flightstatus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_RECORDING_PERMISSIONS_RESULT = 75;
+    private Button textDemoButton;
+    private Button speechDemoButton;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        moveTaskToBack(true);
+    }
+
+    /**
+     * Initializes the application.
+     */
+    private void init() {
+        Log.e(TAG, "Initializing app");
+
+        textDemoButton = (Button) findViewById(R.id.button_select_text);
+        speechDemoButton = (Button) findViewById(R.id.button_select_voice);
+        textDemoButton.setOnClickListener(this);
+        speechDemoButton.setOnClickListener(this);
+
+        // Starting with Marshmallow we need to explicitly ask if we can record audio
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                speechDemoButton.setEnabled(true);
+            } else {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORDING_PERMISSIONS_RESULT);
+            }
+        } else {
+            speechDemoButton.setEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_RECORDING_PERMISSIONS_RESULT) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(),
+                        "LexSample will not be able to use the voice feature", Toast.LENGTH_SHORT).show();
+
+                // Disable the button
+                speechDemoButton.setEnabled(false);
+            } else {
+                speechDemoButton.setEnabled(true);
+            }
+        }
+    }
+
+    /**
+     * On-click listener for buttons text and voice buttons.
+     *
+     * @param v {@link View}, instance of the button component.
+     */
+    @Override
+    public void onClick(final View v) {
+        switch ((v.getId())) {
+
+            case R.id.button_select_text:
+                Intent textIntent = new Intent(this, TextActivity.class);
+                startActivity(textIntent);
+                break;
+
+            case R.id.button_select_voice:
+                Intent voiceIntent = new Intent(this, InteractiveVoiceActivity.class);
+                startActivity(voiceIntent);
+                break;
+        }
+    }
+}
